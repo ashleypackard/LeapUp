580610d0686e33e33f8b894682d9387e42b21ecf
Ashley Packard <packarda1@wit.edu>
2015-02-17 19:59:15 -0500

    make all has_many relations destroy dependent items

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
 	belongs_to :category
 	belongs_to :account
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	validates :title, presence: true, length: {maximum: 75}
 	validates :body, presence: true, length: {maximum: 5000}
